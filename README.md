4f69b1cc17c526f5376db6ac06a0e662f6c12ef2
Gianpaolo Reinares <gprcards@gmail.com>
2025-02-26 09:10:43 -0800

    More or less finished

---
+++ README.md
@@ -0,0 +1,32 @@
+# Advice Generator in React Challenge - Build
+
+### Gianpaolo Reinares
+### 2/26/2025
+# "Advice Generator in React Challenge - Build
+### Description: 
+- With the assets given to you, remake the following Project : Advice Generator.
+
+### Requirements:
+- Built in React
+- Use https://api.adviceslip.com/advice for your API call
+- Must have a component folder ( App cannot be built in the app.js or in the index.js)
+- Responsive to Mobile and Desktop versions
+- Must use all assets given
+- You can use bootstrap or tailwind
+- Goal is to recreate this challenge as close to the design as possible and having it function properly.
+
+### Things to turn in:
+- Peer review
+- Node Modules deleted
+- Azure or Vercel hosted link
+- Github link
+
+
+### Advice Generator in React Challenge - Build_GitHub Repository Link:
+[Advice Generator in React Challenge - Build GitHub Repository Link](https://github.com/MandoxaElemental/Advice-Generator-in-React)
+
+### Advice Generator in React Challenge - Vercel Link:
+[Advice Generator in React Challenge - Vercel Link]()
+
+This has been Peer Reviewed By: 
+> Comments: 
--- src/components/AdviceGenerator.jsx
+++ src/components/AdviceGenerator.jsx
@@ -21,11 +21,18 @@ const AdviceScreen = () => {
     return(
         <>
         <div className='absolute inset-0 flex items-center justify-center font-manrope font-extrabold'>
-            <div className="w-[500px] bg-[#323A49] text-center rounded-2xl">
+            <div className="w-[500px] bg-[#323A49] text-center rounded-2xl mx-5">
                 <p className="text-[#52FFA8] pt-8 tracking-widest text-[12px]">ADVICE: #{number}</p>
                 <h2 className="text-[#CEE3E9] text-[28px] p-8">"{display}"</h2>
-                <div className="flex justify-center pb-5">
-                <img src="images/pattern-divider-desktop.svg" alt="" />
+                <div className="hidden sm:hidden md:block">
+                    <div className="flex justify-center pb-5">
+                    <img src="images/pattern-divider-desktop.svg" alt="pattern-divider" />
+                    </div>
+                </div>
+                <div className="md:hidden">
+                    <div className="flex justify-center pb-5">
+                    <img src="images/pattern-divider-mobile.svg" alt="pattern-divider" />
+                    </div>
                 </div>
                 <div className="flex justify-center">
                 <div onClick={displayTxt} className="relative top-6 rounded-full bg-[#52FFA8] w-12 h-12 flex justify-center items-center hover:shadow-[0px_0px_25px_5px_rgba(82,255,168,1)]">
